9fae01d4bd8e20ec0f00467e24df7c3448362964
Federico Gentile <gentfede+git@gmail.com>
2021-01-17 20:48:01 +0100

    fix: CSS fix, Cap improvements,

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source 'https://rubygems.org'
 
 group :development do
   gem "capistrano", "~> 3.14", require: false
+  gem "ed25519", "~> 1.2", require: false
+  gem "bcrypt_pbkdf", "~> 1.0", require: false
 end
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,8 @@ set :repo_url, "git@github.com:fedus/car-festival.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'main'
+set :repo_tree, 'src'
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"

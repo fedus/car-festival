c3adb203cee9ecb09b2b843f54fb94d746d5e642
Federico Gentile <gentfede+git@gmail.com>
2021-01-17 18:51:04 +0100

    chore: Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.15.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "car-festival"
+set :repo_url, "git@github.com:fedus/car-festival.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+server "dillendapp", user: "deployer", roles: %w{web}
 
 
 # role-based syntax

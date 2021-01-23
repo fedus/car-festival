eab9388de6b1d5d0e26b19f40cd806a93596d291
Federico Gentile <gentfede+git@gmail.com>
2021-01-24 00:07:10 +0100

    chore: explicitly set branch for production deploy cap config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 server "krawallendapp", user: "deploy", roles: %w{web}
-
+set :branch, 'main'
 
 # role-based syntax
 # ==================
